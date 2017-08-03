0424e86c200441e56a621872f9100675ff5f204b
hfrodriguez928 <hayleyrodriguez928@gmail.com>
2017-08-03 14:34:43 -0500

    added survey questions

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // dependencies 
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -9,6 +10,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use(express.static(path.join(__dirname,'./app/public')));
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
